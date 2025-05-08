12557a7b2a2209b8623d366015fc64a5442c9b33
Shaik Karimun <112993544+Karimunshaik@users.noreply.github.com>
2025-05-08 15:22:07 +0530

    Create 1431. Kids With the Greatest Number of Candies.java

---
+++ 1431. Kids With the Greatest Number of Candies.java
@@ -0,0 +1,14 @@
+//1431. Kids With the Greatest Number of Candies
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies=0;
+        for(int candy:candies){
+            maxCandies=Math.max(candy,maxCandies);
+        }
+        List<Boolean> output=new ArrayList<>();
+        for(int candy:candies){
+            output.add(candy+extraCandies>=maxCandies);
+        }
+        return output;
+    }
+}
