fe53d7599ed4c3edcf0ebe37a20a2a4bf5f296fc
Shaik Karimun <112993544+Karimunshaik@users.noreply.github.com>
2025-05-08 15:22:51 +0530

    Update 1431. Kids With the Greatest Number of Candies.java

--- 1431. Kids With the Greatest Number of Candies.java
+++ 1431. Kids With the Greatest Number of Candies.java
@@ -2,9 +2,12 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int maxCandies=0;
+         // Find out the greatest number of candies among all the kids.
         for(int candy:candies){
             maxCandies=Math.max(candy,maxCandies);
         }
+          // For each kid, check if they will have greatest number of candies
+        // among all the kids.
         List<Boolean> output=new ArrayList<>();
         for(int candy:candies){
             output.add(candy+extraCandies>=maxCandies);
